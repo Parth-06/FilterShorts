fc68eef0819a2c388307e04ca90c06375602e390
Parth-06 <parth99.thakkar@gmail.com>
2022-09-23 18:07:01 +0530

    commit

--- app.js
+++ app.js
@@ -1,29 +1,26 @@
-const dotenv = require ('dotenv');
-const express = require('express');
+const dotenv = require("dotenv");
+const express = require("express");
 const app = express();
 
 const cors = require("cors");
 
-dotenv.config({path: "./config.env"})
-require("./DB/conn")
+dotenv.config({ path: "./config.env" });
+require("./DB/conn");
 
-app.use('/uploads',express.static('/uploads'))
+app.use("/uploads", express.static("/uploads"));
 app.use(express.json());
 app.use(cors());
 
-app.use(require('./router/auth'))
-app.use(require('./router/LoginRegiAuth'))
-app.use(require('./router/uploadvideos'))
-// app.use(require('./router/ProfileAuth'))
-app.use(require('./router/userprofileauth.js'))
-// app.use(require('./router/FollowUnFollowAuth.js'))
+app.use(require("./router/auth"));
+app.use(require("./router/LoginRegiAuth"));
+app.use(require("./router/uploadvideos"));
+app.use(require("./router/userprofileauth.js"));
 
-
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static('client/build'))
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
 }
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, ()=>{
-    console.log(`listning At ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`listning At ${PORT}`);
+});
